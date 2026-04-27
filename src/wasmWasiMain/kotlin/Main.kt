7718eb3dae44aa4a07fcc207e70187d81a527c99
Aleksandar Avramovic <aa220216d@student.etf.bg.ac.rs>
2026-04-27 18:57:43 +0200

    Inputing line then printing it

--- src/wasmWasiMain/kotlin/Main.kt
+++ src/wasmWasiMain/kotlin/Main.kt
@@ -8,10 +8,15 @@ import kotlin.wasm.WasmExport
 import kotlin.wasm.unsafe.Pointer
 import kotlin.wasm.unsafe.withScopedMemoryAllocator
 
+private const val STDIN = 0
 private const val STDOUT = 1
 
 @WasmImport("wasi_snapshot_preview1", "fd_write")
 private external fun wasiFdWrite(fd: Int, iovs: Int, iovsLen: Int, nwritten: Int): Int
+
+@WasmImport("wasi_snapshot_preview1", "fd_read")
+private external fun wasiFdRead(fd: Int, iovs: Int, iovsLen: Int, nread: Int): Int
+
 
 fun wasiPrintLn(text : String) = withScopedMemoryAllocator { allocator ->
 
@@ -33,7 +38,7 @@ fun wasiPrintLn(text : String) = withScopedMemoryAllocator { allocator ->
     val written = allocator.allocate(4)
 
     wasiFdWrite(
-        1,
+        STDOUT,
         iov.address.toInt(),
         1,
         written.address.toInt()
@@ -41,8 +46,39 @@ fun wasiPrintLn(text : String) = withScopedMemoryAllocator { allocator ->
 
 }
 
-fun main(){
-    wasiPrintLn("Hello world!");
+fun wasiReadLN() : String = withScopedMemoryAllocator { allocator ->
+    val bufferSize = 1024
+
+    val buffer = allocator.allocate(bufferSize)
+
+    val iov = allocator.allocate(8)
+    Pointer(iov.address).storeInt(buffer.address.toInt())
+    Pointer(iov.address + 4u).storeInt(bufferSize)
+
+    val nread = allocator.allocate(4)
+
+    val result = wasiFdRead(
+        STDIN,
+        iov.address.toInt(),
+        1,
+        nread.address.toInt()
+    )
+
+    val size = Pointer(nread.address).loadInt()
+
+    val bytes = ByteArray(size)
+    for (i in 0 until size) {
+        bytes[i] = Pointer(buffer.address + i.toUInt()).loadByte()
+    }
+
+    val input = bytes.decodeToString()
+
+    return input
+}
+fun main() {
+    val input = wasiReadLN()
+
+    wasiPrintLn(input);
 }
 
 @WasmExport
